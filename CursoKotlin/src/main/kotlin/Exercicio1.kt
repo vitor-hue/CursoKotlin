4afc0a4c42dd33e40cd241b95364f34d87d91f3e
Vitor Alexandre <vitinhofer91@gmail.com>
2022-09-25 18:20:50 -0300

    Primeiro exercício do curso de kotlin
    A resolução deste exercício era para colocar o meu nome completo numa variável imutável, texto vazio em uma variável, o menor tipo de dado possível em um número de calçado, PIB do Brasil, número da população do Brasil e o PIB per capita do Brasil.

---
+++ CursoKotlin/src/main/kotlin/Exercicio1.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val nome = "Vitor Alexandre Fernandes de Lima Silva"
+    var text: String
+    var calcado: Byte = 40
+    var PIB: Long = 1869000000000
+    var populacao = 211000000
+    println("R$ ${PIB / populacao}")
+}
